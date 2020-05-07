f03df7ede1bbd0acd68a31418531c7b6afb9c505
Maticaba99 <maticaba99@hotmail.com>
2020-05-07 06:25:21 -0300

    problema solucionado inicio doble

--- src/View/Public/public.js
+++ src/View/Public/public.js
@@ -3,22 +3,19 @@ import { Layout, Row, Col } from "antd";
 import "antd/dist/antd.css";
 import "./public.css";
 import ContentPublic from "./switchContent";
-import Inicio from '../Public/cie/cie';
 import Nav from "./nav";
 
 const { Header, Content, Footer } = Layout;
 
- class Public extends React.Component {
+class Public extends React.Component {
   render() {
     return (
       <Layout>
         <Header className="header">
           <Nav />
-        
         </Header>
         <Content className="content">
-        <ContentPublic />
-          <Inicio/>
+          <ContentPublic />
         </Content>
         <Footer className="footer">
           <Row>
