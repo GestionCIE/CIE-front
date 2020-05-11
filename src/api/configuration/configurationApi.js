89ee185d7a713a384ba9436f24dbe95350544a67
Cristian Vargas <cvg97@misena.edu.co>
2020-05-11 00:45:46 -0500

    nueva estructura de carpetas para las llamadas a la API

---
+++ src/api/configuration/configurationApi.js
@@ -0,0 +1,30 @@
+
+ class ConfigurationApi{
+    getAllSystemModules(){
+        return new Promise((resolve , reject )=>{
+            fetch('http://localhost:3005/config/getSystemModules')
+                .then(res=> res.json())
+                .then((response)=>{
+                    resolve(response);
+            });
+        })
+    }
+
+    updateVisibleModule(data){
+        return new Promise((resolve, reject)=>{
+            fetch('http://localhost:3005/config/updateVisible', {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers:{
+                    'Content-Type': 'application/json'
+                }})
+                .then(res=> res.json())
+                .then((response)=>{
+                    resolve(response);
+                });
+        });
+    }
+
+}
+
+export default ConfigurationApi; 
