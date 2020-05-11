e1c71325464239831404373591bdae4103dd15da
Cristian Vargas <cvg97@misena.edu.co>
2020-05-11 01:38:26 -0500

    fixed conflicts

--- src/components/configuration/configurationComponent.js
+++ src/components/configuration/configurationComponent.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import {Layout, Row, Col, Table, Button, Select, message} from 'antd';
-
+import {Layout, Row, Col, Table, Button, Select, message, Collapse, List, Switch} from 'antd';
+import configurationApi from './../../api/configuration/configurationApi';
 const {Option} = Select; 
 const {Content} = Layout;
+const {Panel} = Collapse;
+const api = new configurationApi(); 
 class configurationComponent  extends React.Component{
 
     state = {
-        data: []
+        data: [],
+        moduleAdviser: [],
+        moduleAssistant: [],
+        moduleEntrepreneur: []
+
     };
 
     componentDidMount(){
         this.getAllUsers();
+        this.getSystemModules();
     }
 
     getAllUsers(){
@@ -76,6 +83,24 @@ class configurationComponent  extends React.Component{
         </Select>; 
         return select;
     }
+
+    async getSystemModules(){
+       const response = await api.getAllSystemModules();
+
+       this.setState({moduleAdviser : response.result.adviser, 
+        moduleAssistant: response.result.assistant,
+        moduleEntrepreneur: response.result.entrepreneur});
+    }
+
+     deactivate = async (state, id)=> {
+
+        const response = await api.updateVisibleModule({visible: state, id: id });
+        if(response.result == 'edited') {
+            message.success("Se cambio el estado del modulo");
+            this.getSystemModules();
+        }
+    }
+
     render(){
         const columns = [{
             title: 'Nombre',
@@ -104,6 +129,44 @@ class configurationComponent  extends React.Component{
             <Row>
                 <Col span={12}>
                     <Table size="small" rowKey={ recoder => recoder.idUsers} columns={columns} dataSource={this.state.data}/>            
+                </Col>
+                <Col offset={3}>
+                    <Collapse accordion>
+                        <Panel header="Activacion de los Modulo del Asistente">
+                        <List dataSource={this.state.moduleAssistant}
+                            renderItem={item => (
+                                <List.Item>
+                                    <span>{item.name}</span>
+                                    <Switch key={item.idSystemModules} onChange={(e)=>this.deactivate(e, item.id )} checked={item.active} />
+                                </List.Item>
+                            )}
+                        />
+                        </Panel> 
+                    </Collapse>
+                    <Collapse accordion>
+                        <Panel header="Activacion de los Modulo del asesor">
+                        <List dataSource={this.state.moduleAdviser}
+                            renderItem={item => (
+                                <List.Item>
+                                    <span>{item.name}</span>
+                                    <Switch key={item.idSystemModules} onChange={(e)=>this.deactivate(e, item.id )} checked={item.active} />
+                                </List.Item>
+                            )}
+                        />
+                        </Panel> 
+                    </Collapse>
+                    <Collapse accordion>
+                        <Panel header="Activacion de los Modulo del emprendedor">
+                        <List dataSource={this.state.moduleEntrepreneur}
+                            renderItem={item => (
+                                <List.Item>
+                                    <span>{item.name}</span>
+                                    <Switch key={item.idSystemModules} onChange={(e)=>this.deactivate(e, item.id )} checked={item.active} />
+                                </List.Item>
+                            )}
+                        />
+                        </Panel> 
+                    </Collapse>
                 </Col>
             </Row>
         </Content>
