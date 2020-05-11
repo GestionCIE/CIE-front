ee50de92bca1ad8c2db16de21df7d998860d0fe1
Maticaba99 <maticaba99@hotmail.com>
2020-05-11 10:17:04 -0300

    esqueleto y ruta de quienes somos agregado

--- src/View/Public/switchContent.js
+++ src/View/Public/switchContent.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import {Route, Switch} from 'react-router-dom';
-import Register from './login/register';
-import Login from './login/login';
-import Inicio from './cie/cie';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Register from "./login/register";
+import Login from "./login/login";
+import Inicio from "./cie/cie";
+import QuienesSomos from "../Public/QuienesSomos/QuienesSomos";
 
-class Content extends React.Component{
-    render(){
-        return(
-        <Switch>
-            <Route exact path="/inicio" component={Inicio}></Route>
-            <Route exact path="/inicio/signup" component={Login}></Route>
-            <Route exact path="/inicio/signin" component={Register}></Route>
-        </Switch>     
-        );
-    }
+class Content extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/inicio" component={Inicio}></Route>
+        <Route exact path="/inicio/signup" component={Login}></Route>
+        <Route exact path="/inicio/signin" component={Register}></Route>
+        <Route
+          exact
+          path="/inicio/quienessomos"
+          component={QuienesSomos}
+        ></Route>
+      </Switch>
+    );
+  }
 }
-
 
 export default Content;
