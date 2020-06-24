96cb135b9475f6f605b0a5b5bde733c0a4e61788
Cristian Vargas <vargascristian@americana.edu.co>
2020-06-24 17:26:17 -0500

    Update router.js

--- src/routers/router.js
+++ src/routers/router.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import history from "./history";
 
-import Public from "../View/Public/public";
-import Home from "../View/Home/Home";
+import Public from "../view/Public/public";
+import Home from "../view/Home/Home";
 
 function RouterMain() {
   return (
