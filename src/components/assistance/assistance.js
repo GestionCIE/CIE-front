42599c7b7e8b57495d171d09d37477534b22db83
Cristian Vargas <vargascristian@americana.edu.co>
2020-06-27 10:54:32 -0500

    Nuevos cambios, se unen 3 modulos asistencia, trazabilidad y reportes, se agrega la libreria chart.js para graficas

--- src/components/assistance/assistance.js
+++ src/components/assistance/assistance.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import {Row, Col, Select, Table, Checkbox} from 'antd';
+import {Row, Col, Select, Table, Checkbox, Tabs } from 'antd';
 import assistenceApi from '../../api/assistance/assistanceApi';
+import TracingComponent from './../seguimiento/tracingComponent';
 import './assistance.css';
+import Charts from './charts';
+import assistantImg from '../../assets/assistant.svg';
 const {Option} = Select;
+const {TabPane} = Tabs;
 const api = new assistenceApi();
 
 class Assistance extends React.Component{
 
     state = {
         events : [],
-        attendance: []
+        attendance: [],
+        value: false
     };
 
     componentDidMount(){
@@ -57,21 +62,38 @@ class Assistance extends React.Component{
         return (
             <Row>
                 <Col span={24}>
-                    <h6>Lista de asistencia a los eventos</h6>
+                    <h6>Trazabilidad de eventos</h6>
                 </Col>
                 <Col span={24}>
-                  <Select className="Select_Assistance" defaultValue="Seleccionar un evento" onChange={this.getAssistenceByEvent} >
-                      {this.state.events.map(e=>{
-                          return <Option value={e.idEvents}> {e.eventName} </Option>
-                      })}
-                  </Select>  
-                </Col>
-                <Col>
-                      {this.state.attendance.length > 0 ? 
-                         <Table columns={columns} dataSource={this.state.attendance}>
+                <Tabs >
+                    <TabPane tab="Asistencia a eventos" key="1">
+                        <Row>
+                            <Col span={24}>
+                                <h6>Lista de asistencia a los eventos</h6>
+                            </Col>
+                            <Col span={24}>
+                                <Select className="Select_Assistance" defaultValue="Seleccionar un evento" onChange={this.getAssistenceByEvent} >
+                                {this.state.events.map(e=>{
+                                    return <Option value={e.idEvents}> {e.eventName} </Option>
+                                })}
+                                </Select>  
+                            </Col>
+                            <Col>
+                            {this.state.attendance.length > 0 ? 
+                                <Table columns={columns} dataSource={this.state.attendance}>
 
-                         </Table> : null
-                      }
+                                </Table> : <img src={assistantImg} className="Img_Assistant" />
+                            }
+                            </Col>
+                        </Row>
+                    </TabPane>
+                    <TabPane tab="Seguimiento" key="2">
+                        <TracingComponent/>
+                    </TabPane>
+                    <TabPane tab="Reportes" key="3">
+                        <Charts />
+                    </TabPane>
+                </Tabs>
                 </Col>
             </Row>
         )
@@ -79,3 +101,4 @@ class Assistance extends React.Component{
 }
 
 export default Assistance;
+
